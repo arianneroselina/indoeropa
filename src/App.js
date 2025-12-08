3f58d7cd94eeca4a889e879f975ecea7d6a39ce4
pranantoa <arianne.prananto@cosateq.com>
2025-12-08 20:39:47 +0700

    initial commit with react and tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-4xl font-bold">Welcome to My Website</h1>
+            <p>This is a basic React app with TailwindCSS!</p>
+        </div>
+    );
+}
+
+export default App;
